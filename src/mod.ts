73f0fc45d8b163cf77e105d1df5a3e9410d5abfa
Victor Nascimento Bakke <gipphe@gmail.com>
2024-09-27 22:41:41 +0200

    chore: improve logging

--- src/mod.ts
+++ src/mod.ts
@@ -3,19 +3,18 @@ import fs from "node:fs";
 import { ITemplateItem } from "@spt/models/eft/common/tables/ITemplateItem";
 import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
-import { VFS } from "@spt/utils/VFS";
 import { DependencyContainer } from "tsyringe";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 
 export class Main implements IPostDBLoadMod {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   private config = require("../config/config.json");
+  private logger: ILogger = null;
 
   public postDBLoad(container: DependencyContainer): void {
-    const logger = container.resolve<ILogger>("WinstonLogger");
+    this.logger = container.resolve<ILogger>("WinstonLogger");
     const db = container.resolve<DatabaseServer>("DatabaseServer");
     const tables = db.getTables();
-    const vfs = container.resolve<VFS>("VFS");
     const { items } = tables.templates;
     const itemsToDump: string[] = this.config.item_ids;
     const dump: Record<string, ITemplateItem> = Object.entries(items).reduce(
@@ -29,21 +28,39 @@ export class Main implements IPostDBLoadMod {
     );
     const keys = Object.keys(dump);
     const missing = itemsToDump.filter((id) => !keys.includes(id));
-    logger.info(`${keys.length} matching items found`);
     if (missing.length > 0) {
-      logger.warning(
+      this.warning(
         `${missing.length} IDs were not found.\nCould not find templates with the following IDs:\n${missing.join(", ")}`,
       );
     }
+
+    if (keys.length <= 0) {
+      this.warning("No matching items found. Nothing to dump.");
+      return;
+    }
+    this.info(`${keys.length} matching items found`);
     const dest = path.resolve(__dirname, "../data", `${Date.now()}.json`);
     const dir = path.dirname(dest);
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, { recursive: true });
-      logger.debug(`Created directory at ${dir}`);
+      this.debug(`Created directory at ${dir}`);
     }
 
     fs.writeFileSync(dest, JSON.stringify(dump));
-    logger.info(`Dumped retrieved templates into ${dest}`);
+    this.info(`Dumped retrieved templates into ${dest}`);
+  }
+
+  private info(msg: string): void {
+    this.log(this.logger.info, msg);
+  }
+  private warning(msg: string): void {
+    this.log(this.logger.warning, msg);
+  }
+  private debug(msg: string): void {
+    this.log(this.logger.debug, msg);
+  }
+  private log(logFn: (m: string) => void, msg: string): void {
+    logFn(`[SPT-DUMP-ITEMS]: ${msg}`);
   }
 }
 
